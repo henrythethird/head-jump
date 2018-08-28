0d8b69750e90138e7492d44093e2ba4f4cce1dd8
Daniel Lehrner <daniel.lehrner@pwc-digital.ch>
2018-08-28 13:59:16 +0200

    added webpack, basic browser websocket

---
+++ frontend/webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  mode: "development",
+  devServer: {
+    port: 8000
+  },
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ]
+  }
+}
